5ff170a51765fee39ce7ba91492735cd0ff61748
deepak-singh5219 <84671689+deepak-singh5219@users.noreply.github.com>
2022-08-09 15:44:26 +0530

    more setup

---
+++ components/index.js
@@ -0,0 +1,13 @@
+
+
+export {default as Footer} from './Footer';
+export {default as Cart} from './Cart';
+export {default as FooterBanner} from './FooterBanner';
+export {default as Product} from './Product';
+export {default as Navbar} from './Navbar';
+export {default as HeroBanner} from './HeroBanner';
+export {default as Layout} from './Layout';
+
+
+
+ 
