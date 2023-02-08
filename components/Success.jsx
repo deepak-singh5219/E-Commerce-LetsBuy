2b736d3adbf6e02709a4d850329817334f306e60
Deepak Singh <singhdeepak5219@gmail.com>
2023-02-08 22:30:50 +0530

    success page

--- components/Success.jsx
+++ components/Success.jsx
@@ -1,8 +1,29 @@
+import Image from 'next/image'
 import React from 'react'
+import image from '../assets/successImg.png'
+
+
 
 const Success = () => {
   return (
-    <div>Success Page</div>
+  <div style={{
+    'display':'flex',
+    'justifyContent':'center',
+    'alignItems':'center',
+    'flexDirection':'column'
+  }}>
+    <img src={image.src} alt="" className="logo-container" style={{ width:'400px'}} />
+
+    <p style={{
+      'text-align':'center',
+      'fontSize':'30px',
+      'margin':'50px'
+
+  }}>
+    Order Successfull 🥳
+    </p>
+
+  </div>
   )
 }
 
